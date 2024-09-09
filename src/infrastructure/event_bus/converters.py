1a88cce1a8241af59621485e30bf95b8a46af65c
daron035 <kamil249@mail.ru>
2024-09-09 07:36:09 +0500

    add UserCreated event

--- src/infrastructure/event_bus/converters.py
+++ src/infrastructure/event_bus/converters.py
@@ -1,10 +1,11 @@
 from src.application.common.exceptions import MappingError
 from src.domain.common.events.event import Event
 from src.domain.messages.events import ChatCreated, MessageCreated
+from src.domain.user.events import UserCreated
 from src.infrastructure.event_bus import events as integration_events
 
 
-DomainEvents = Event | ChatCreated | MessageCreated
+DomainEvents = Event | ChatCreated | MessageCreated | UserCreated
 
 
 def convert_chat_created_to_integration(
@@ -23,6 +24,17 @@ def convert_message_created_to_integration(
     )
 
 
+def convert_user_created_to_integration(
+    event: UserCreated,
+) -> integration_events.UserCreated:
+    return integration_events.UserCreated(
+        user_id=event.user_id,
+        username=event.username,
+        first_name=event.first_name,
+        last_name=event.last_name,
+    )
+
+
 def convert_domain_event_to_integration(
     event: DomainEvents,
 ) -> integration_events.IntegrationEvent:
@@ -31,5 +43,7 @@ def convert_domain_event_to_integration(
             return convert_chat_created_to_integration(event)
         case MessageCreated():
             return convert_message_created_to_integration(event)
+        case UserCreated():
+            return convert_user_created_to_integration(event)
         case _:
             raise MappingError(f"Event {event} cannot be mapped to integration event")
--- src/infrastructure/event_bus/events/__init__.py
+++ src/infrastructure/event_bus/events/__init__.py
@@ -1,10 +1,12 @@
 from .base import IntegrationEvent
 from .chat_created import ChatCreated
 from .message_created import MessageCreated
+from .user_created import UserCreated
 
 
 __all__ = (
     "IntegrationEvent",
     "ChatCreated",
     "MessageCreated",
+    "UserCreated",
 )
